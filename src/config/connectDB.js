dd46a229a80abb22b7a74d0ed1048dfa55fac99e
hieupxtn <83078842+hieupxtn@users.noreply.github.com>
2025-06-05 23:25:52 +0700

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -11,7 +11,7 @@ const { Sequelize } = require('sequelize');
 // });
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('store-electronics', 'root', null, {
+const sequelize = new Sequelize('store_app_db', 'root', null, {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
